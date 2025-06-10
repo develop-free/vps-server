546af5eaa0c81c1d16c2bee5d09f11088f1615f5
Владислав <vlad1891tg@gmail.com>
2025-06-11 01:51:29 +0700

    done

--- src/config/ecosystem.config.js
+++ src/config/ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'server',
     script: '../server.js', // Укажите ваш основной файл
     env: {
+      mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/SITES',
       EMAIL_USER: 'CyberCats.kpk@gmail.com',
       EMAIL_APP_PASS: 'udjztryefbiwyxs'
     }
