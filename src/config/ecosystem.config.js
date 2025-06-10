9656993e959f1552498807d41413e662f8098072
Владислав <vlad1891tg@gmail.com>
2025-06-11 03:07:58 +0700

    done

--- src/config/ecosystem.config.js
+++ src/config/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'server',
-    script: 'src/server.js', // Укажите ваш основной файл
+    script: './src/server.js', // Укажите ваш основной файл
     env: {
       MONGO_URI: 'mongodb+srv://admin:admin@cluster0.sfjy9.mongodb.net/SITES?retryWrites=true&w=majority&appName=Cluster0',
       EMAIL_USER: 'CyberCats.kpk@gmail.com',
